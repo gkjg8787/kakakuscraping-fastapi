fa8e5b699191b4a9d79bdda2ebfaf29bffa9a046
gkjg8787 <gkjgfafasf@gmail.com>
2023-09-06 19:19:00 +0900

    add reset table id sequence for postgre to sqlite db tool

--- kakaku/tool/postgre_import_csv.sh
+++ kakaku/tool/postgre_import_csv.sh
@@ -1,8 +1,23 @@
 tables=(
-"items" "url" "siteupdate"
-"itemsprice" "pricelog_2days" "newestitem"
-"groups" "groupsitem"
-"stores" "store_postage"
+"items"
+"url"
+"siteupdate"
+"itemsprice"
+"pricelog_2days"
+"newestitem"
+"groups"
+"groupsitem"
+"stores"
+"store_postage"
+)
+seq_tables=(
+  "items:items_item_id"
+  "url:url_url_id"
+  "itemsprice:itemsprice_log_id"
+  "pricelog_2days:pricelog_2days_log_id"
+  "newestitem:newestitem_item_id"
+  "groups:groups_group_id"
+  "stores:stores_store_id"
 )
 dbuser="dbuser"
 dbname="appdb"
@@ -13,7 +28,13 @@ parentdirname="/tmp"
 importdir="${parentdirname}/${importarchive}"
 
 import_csv(){
-psql -U ${dbuser} -d ${dbname} -c "\copy ${1} from ${importdir}/${1}${fileextend} with csv header"
+  psql -U ${dbuser} -d ${dbname} -c "\copy ${1} from ${importdir}/${1}${fileextend} with csv header"
+}
+reset_seq(){
+  t_base=${1%:*}
+  t_seq=${1#*:}
+  t_clm=${t_seq#*${t_base}_}
+  psql -U ${dbuser} -d ${dbname} -c "SELECT SETVAL('${t_seq}_seq', (SELECT MAX(${t_clm}) FROM ${t_base}) )"
 }
 
 if [ ! -f ${importdir}${archiveextend} ]; then
@@ -28,6 +49,10 @@ tar zxf ${importdir}${archiveextend}
 for i in "${!tables[@]}";
 do
   import_csv "${tables[$i]}"
+done
+for i in "${!seq_tables[@]}";
+do
+  reset_seq "${seq_tables[$i]}"
 done
 
 rm ${importarchive}/*${fileextend}
